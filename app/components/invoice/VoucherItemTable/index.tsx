cbee0ff64af8353c37ec0764653201f329278614
Tong Tat Thanh <thanh.tong@.com>
2025-05-14 22:12:32 +0700

    update total voucher form

--- app/components/invoice/VoucherItemTable/index.tsx
+++ app/components/invoice/VoucherItemTable/index.tsx
@@ -9,10 +9,10 @@ export const VoucherItemTable = () => {
         <PaymentVoucherItem />
       </section>
       <section className="space-y-4">
-        <div className="flex flex-col gap-3 items-end">
-          <div className="flex justify-between w-full max-w-xs text-lg bg-neutral-50 p-2 rounded-lg ">
-            <span className="font-medium text-base text-gray-700">Total:</span>
-            <span className="font-bold text-gray-900">
+        <div className="flex flex-col items-end border-t border-neutral-200 w-[300px] ml-auto p-2 px-4 gap-1">
+          <div className="flex justify-between w-full max-w-xs text-lg  ">
+            <span className=" text-sm text-gray-500 font-medium">Total</span>
+            <span className="font-semibold text-gray-700">
               {watch("details.subTotal") || 0}
             </span>
           </div>
